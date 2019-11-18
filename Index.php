da9ca56cf49558bd3aea1a68ff51e0839273259c
Bruxo84 <57851618+Bruxo84@users.noreply.github.com>
2019-11-18 12:50:22 -0300

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,3 @@
+<?php
+$pagina = '<!DOCTYPE HTML><html lang="es"><head><title>Ejemplo de página</title><meta charset="UTF-8"><meta name="description" content="Descripción de la página"></head><body><h1>Hola Mundo!</h1><p>Este es un ejemplo de una página de prueba. A través de vagrant se levantó un Ubuntu, se le configuró apache como servidor web y PHP como lenguaje de servidor</p></body></html>';
+echo $pagina;
